309f20ddfc3e01ffc3a1c9b4213d38ca4341a62b
Lovas Istvan <l.pista@hotmail.com>
2013-04-21 22:32:32 +0200

    createk kész

--- Scriptek/1_create_tables.sql
+++ Scriptek/1_create_tables.sql
@@ -1,30 +1,38 @@
 DROP TABLE Lakók;
 CREATE TABLE Lakók (
-	Név							VARCHAR2,
+	Név							VARCHAR2(30),
 	Egészségbiztosítási_szám	VARCHAR2(9),
 	SzületésiDátum				DATE,
 	Nem							CHAR(1) CHECK( Nem IN ('f','n') ),
-	Személyigazolvány_szám		VARCHAR2(11),
+	Személyigazolvány_szám		VARCHAR2(11) UNIQUE,
 	Tartózkodas_tól				DATE,
 	Tartózkodas_ig				DATE,
 	Ország						VARCHAR2(50),
 	ház_szám					NUMBER(4),
 	Csoport_név					VARCHAR2(50),
-	Csoportvezetõ				
+	Csoportvezetõ				VARCHAR2(30)
 )
-
-ALTER TABLE Lakók 
 
 DROP TABLE Csoportok;
 CREATE TABLE Csoportok(
 	csoport_név			VARCHAR2(50),
-	csoportvezetõ		,
+	csoportvezetõ		VARCHAR2(30),
 	csoport_pontszám	INTEGER
 )
 
 DROP TABLE Házak;
 CREATE TABLE Házak (
 	ház_szám		NUMBER(4),
-	ház_megnevezés	VARCHAR2(50),
+	ház_megnevezés	VARCHAR2(25) NOT NULL,
 	férõhely_szám	NUMBER(4)
 )
+
+ALTER TABLE Lakók ADD CONSTANT p_Lakók PRIMARY KEY (Név,Egészségbiztosítási_szám);
+ALTER TABLE Csoportok ADD CONSTANT p_Csoportok PRIMARY KEY (csoport_név,csoportvezetõ);
+ALTER TABLE Házak ADD CONSTANT p_Házak PRIMARY KEY (ház_szám)
+
+ALTER TABLE Lakók ADD CONSTANT f_Lakók_Csoportok FOREIGN KEY (Csoport_név,Csoportvezetõ)
+	REFERENCES Csoportok (Csoport_név,Csoportvezetõ);
+	
+ALTER TABLE Lakók ADD CONSTANT f_Lakók_Házak FOREIGN KEY (ház_szám)
+	REFERENCES Házak (ház_szám);
