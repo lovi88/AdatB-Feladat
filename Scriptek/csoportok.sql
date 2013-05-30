956320ebbadf8b5feaaca53e894fadbde819ed2d
lovi88 <l.pista@hotamail.com>
2013-05-31 01:03:32 +0200

    menűk szépítése

--- Scriptek/csoportok.sql
+++ Scriptek/csoportok.sql
@@ -1,14 +1,16 @@
 
-PROMPT Táborhely Üzemeltető Csoportok almenű
+PROMPT Táborhely Üzemeltető: Csoportok almenű
 PROMPT
-PROMPT ***********************************************************
-PROMPT *  Csoportok listázása (csoport_lista)                    *
-PROMPT *  Új Csoport felvétele (csoport_új)                      *
-PROMPT *  Csoport módosítása (csoport_mód)                       *
-PROMPT *  Csoport törlése (csoport_töröl)                        *
-PROMPT *                                                         *
-PROMPT *  Kiválasztott csoport átlaga (csoport_kiv_atlag)        *
-PROMPT ***********************************************************
+PROMPT *************************************************************
+PROMPT *  Csoportok listázása                  (csoport_lista)     *
+PROMPT *  Új Csoport felvétele                 (csoport_új)        *
+PROMPT *  Csoport módosítása                   (csoport_mód)       *
+PROMPT *  Csoport törlése                      (csoport_töröl)     *
+PROMPT *                                                           *
+PROMPT *  Kiválasztott csoportvezető átlaga    (csoport_kiv_atlag) *
+PROMPT *  Az első öt csoport                   (csoport_első_öt)   *
+PROMPT *                                                           *
+PROMPT *************************************************************
 
 ACCEPT al PROMPT 'A kívánt almenű eléréséhez írjon be, egy a zárójelekben lévő szót:'
 @@ &al
--- Scriptek/menu.sql
+++ Scriptek/menu.sql
@@ -1,10 +1,10 @@
-PROMPT Táborhely Üzemeltetõ Fõmenõ
+PROMPT Táborhely Üzemeltetõ: Fõmenõ
 PROMPT
 PROMPT *************************************************
-PROMPT *  Házak kezelése (házak)                       *
-PROMPT *  Coportok kezelése (csoportok)                *
-PROMPT *  Lakók kezelése (lakók)                       *
-PROMPT *  DDL-Táblamódosítás (ddl)                     *
+PROMPT *  Házak kezelése         (házak)               *
+PROMPT *  Coportok kezelése      (csoportok)           *
+PROMPT *  Lakók kezelése         (lakók)               *
+PROMPT *  DDL-Táblamódosítás     (ddl)                 *
 PROMPT *************************************************
 
 ACCEPT al PROMPT 'A kívánt almenû eléréséhez írjon be, egy a zárójelekben lévõ szót:'
